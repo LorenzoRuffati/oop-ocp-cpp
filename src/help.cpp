c5182511169976bc88f4642d3e29977537ea0a0a
Lorenzo Ruffati <lorenzo.ruffati@unikie.com>
2022-09-30 12:40:38 +0300

    Set bazel structure

---
+++ src/help.cpp
@@ -0,0 +1,4 @@
+#include "help.h"
+int mult(){
+    return 7*6;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "help.h"
+
+int main() {
+    std::cout << "Hello World! " << mult();
+    return 0;
+}
